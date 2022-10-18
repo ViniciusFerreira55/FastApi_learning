164c4b9ec0719c12cd9d7f5deae96e0079ededc8
Ferreira Vinicius (CaP/ETS) <fev5ca@bosch.com>
2022-10-18 15:31:38 -0300

    continue to learning fastapi :)

--- main.py
+++ main.py
@@ -1,8 +1,38 @@
+#Here i import the fastApi, you must
+#install uvicorn[standard] and fastapi with pip
+#All credits to "Amigoscode" who make this code
+#https://www.youtube.com/c/amigoscode
+from uuid import uuid4, UUID
 from fastapi import FastAPI
+from typing import List
+from models import Gender, User, Role
 
-
+#here we call the fastApi function
 app = FastAPI() 
+#here we pass the root of fastapi
+
+db: List[User] = [
+    User(id=UUID('817d6f0d-b3e0-4af7-bf3c-4f3e89c2f1b8'), 
+    first_name="Belle", 
+    last_name="Bellinha", 
+    gender=Gender.female,
+    roles=[Role.student]),
+    User(id=UUID('7508a784-1f16-49ef-9145-77cd79f747fa'),
+    first_name="Wilson",
+    last_name="delas", 
+    middle_name="rei",
+    gender=Gender.male,
+    roles=[Role.admin, Role.user])
+]
 
 @app.get("/")
-def root():
-    return {"Hello": "World"}
+async def root():
+    return {"Hello": "Mundo"}
+
+@app.get("/api/v1/users")
+async def fetch_users():
+    return db;
+
+@app.post("/api/v1/users")
+async def register_user(user: User):
+    db.append(user)
---
+++ models.py
@@ -0,0 +1,23 @@
+from typing import List, Optional, List
+from uuid import UUID, uuid4
+from pydantic import BaseModel
+from enum import Enum
+#in this file i am making the base
+#of the user infos
+
+class Gender(str, Enum):
+    male = 'male'
+    female = 'female'
+
+class Role(str, Enum):
+    admin = 'admin'
+    user = 'user'
+    student = 'student'
+
+class User(BaseModel):
+    id: Optional[UUID] = uuid4()
+    first_name: str
+    last_name:str
+    middle_name: Optional[str]
+    gender: Gender
+    roles: List[Role]
